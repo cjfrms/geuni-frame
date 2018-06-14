9ce19c000dcb9d244fc8b5b5ef18648cb28f6c66
geuni <cjfrms@live.com>
2018-06-14 21:46:04 +0800

    1

--- backend/src/main/java/com/geuni/conf/WebSecurityConfig.java
+++ backend/src/main/java/com/geuni/conf/WebSecurityConfig.java
@@ -3,10 +3,8 @@ package com.geuni.conf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -21,6 +19,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/img/*.jpg").permitAll()
+                .antMatchers("/img/*.png").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/js/**").permitAll()
                 .antMatchers("/*", "/index").permitAll()
                 .anyRequest().authenticated()
                 .and()
